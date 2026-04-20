d903689767dc90aeb36e89c746f9ea7d67d94521
Victoria Kilgore <151574846+kilgorev@users.noreply.github.com>
2026-04-20 13:50:44 -0400

    added game_engine

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,8 +3,28 @@ import { HashRouter, Routes, Route, Link } from 'react-router-dom'
 import giraffeImg from './assets/giraffe.jpeg'
 import hopcatImg from './assets/hopcat.jpeg'
 import stadiumImg from './assets/stadium.jpeg'
-import githubImg from './assets/github.png'
+import linkedIcon from './assets/linkedin-2.svg'
+import itchIcon from './assets/itch.svg'
+import bookIcon from './assets/book.svg'
+import cinderellaCover from './assets/cinderella_cover.png'
+import battleCover from './assets/battle-sim.jpeg'
+import latchawCover from './assets/latchaw.png'
+import engineCover from './assets/game_engine.png'
 import './App.css'
+
+const cindDesc = "This was a project made for my Multimedia Project in my German Fairy Tales course.  While short, the project itself was meant to showcase my dialogue creation and state management skills. All assets were produced and created by myself (other than the music)."
+
+function Header() {
+  return (
+    <header className='header'>
+      <Link to="/">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 128 128" className='button-icon' id="social">
+            <path d="M 70 18.074219 C 69.225 18.074219 68.450391 18.350391 67.900391 18.900391 L 14.099609 72.599609 C 12.899609 73.799609 12.899609 75.700781 14.099609 76.800781 L 22.599609 85.300781 C 23.199609 85.900781 23.899219 86.199219 24.699219 86.199219 C 25.499219 86.199219 26.300781 85.900781 26.800781 85.300781 L 70 42.199219 L 102 74.199219 L 102 114.5 C 99.1 115.5 96.899219 118 96.199219 121 L 84 121 L 84 80 C 84 78.3 82.7 77 81 77 L 59 77 C 57.3 77 56 78.3 56 80 L 56 121 L 38 121 L 38 94 C 38 92.3 36.7 91 35 91 C 33.3 91 32 92.3 32 94 L 32 101.40039 C 27.1 102.60039 23.300781 106.40078 22.300781 111.30078 C 17.400781 112.30078 13.500391 116.2 12.400391 121 L 4 121 C 2.3 121 1 122.3 1 124 C 1 125.7 2.3 127 4 127 L 99 127 C 100.7 127 102 125.7 102 124 L 102 123 C 102 121.3 103.3 120 105 120 C 106.7 120 108 121.3 108 123 L 108 124 C 108 125.7 109.3 127 111 127 L 114 127 C 115.7 127 117 125.7 117 124 C 117 122.3 115.7 121 114 121 L 113.80078 121 C 113.10078 118 110.9 115.5 108 114.5 L 108 80.199219 L 113.09961 85.300781 C 113.69961 85.900781 114.39922 86.199219 115.19922 86.199219 C 115.99922 86.199219 116.80078 85.900781 117.30078 85.300781 L 125.80078 76.800781 C 127.00078 75.600781 127.00078 73.699609 125.80078 72.599609 L 72.099609 18.900391 C 71.549609 18.350391 70.775 18.074219 70 18.074219 z M 70 25.300781 L 119.5 74.800781 L 115.30078 79 L 72.099609 35.900391 C 71.499609 35.300391 70.8 35 70 35 C 69.2 35 68.500391 35.300391 67.900391 35.900391 L 24.699219 79 L 20.5 74.800781 L 70 25.300781 z M 64 101 C 65.7 101 67 102.3 67 104 C 67 105.7 65.7 107 64 107 C 62.3 107 61 105.7 61 104 C 61 102.3 62.3 101 64 101 z M 32 107.69922 L 32 121 L 18.699219 121 C 19.799219 118.6 22.2 117 25 117 C 26.7 117 28 115.7 28 114 C 28 111.2 29.6 108.79922 32 107.69922 z M 124 121 A 3 3 0 0 0 121 124 A 3 3 0 0 0 124 127 A 3 3 0 0 0 127 124 A 3 3 0 0 0 124 121 z"></path>
+          </svg>
+      </Link>
+    </header>
+  )
+}
 
 function Home() {
 
@@ -47,10 +67,11 @@ function Home() {
 
 
       <section id="spacer"></section>
-      
 
       <section id="portfolio">
+        <img className="icon" src={bookIcon} role="presentation" aria-hidden="true" />
         <h2>Portfolio</h2>
+        <p>Interact with my work!</p>
         <div className = "navigate">
           <ul>
             <li>
@@ -59,10 +80,12 @@ function Home() {
               </Link>
             </li>
             <li>
+              <Link to="/softwaredev">
               Software Development
+              </Link>
             </li>
             <li>
-              Concepts
+              Concepts & Art
             </li>
           </ul>
         </div>
@@ -91,25 +114,25 @@ function Home() {
             </li>
             <li>
               <a href="https://lollipopak.itch.io/" target="_blank">
-                <svg
+                <img
                   className="button-icon"
                   role="presentation"
                   aria-hidden="true"
+                  src={itchIcon}
                 >
-                  <use href="src/assets/itch.svg"></use>
-                </svg>
+                  </img>
                 itch.io
               </a>
             </li>
             <li>
               <a href="https://www.linkedin.com/in/victoria-kilgore-649888297/" target="_blank">
-                <svg
+                <img
                   className="button-icon"
                   role="presentation"
                   aria-hidden="true"
+                  src={linkedIcon}
                 >
-                  <use href="src/assets/linkedin-2.svg"></use>
-                </svg>
+                  </img>
                 LinkedIn
               </a>
             </li>
@@ -127,17 +150,130 @@ function GameDev(){
 
   return(
     <>
+    <Header/>
     <h1> Game Development </h1>
-    <p>Check out my personal projects!</p>
+    <h2>Check out my game collection!</h2>
 
     <section>
-      <div>
-        Game 1
+      <Link to="/cinderella">
+      <div className='game_article'>
+        <img src={cinderellaCover} className = "cover"></img>
+        <div>
+        <h3>Cinderella: A Visual Novel</h3>
+  
+        <p>{cindDesc}</p>
+        </div>
       </div>
+      </Link>
     </section>
     </>
   )
 
+}
+
+function Software(){
+  return(
+  <>
+  <Header/>
+  <h1> Software Development </h1>
+  <p> These are my software projects!</p>
+  <section>
+    <Link to="/lollipopak_engine">
+      <div className='game_article'>
+        <img src={engineCover} className = "cover"></img>
+        <div>
+        <h3>Game Engine</h3>
+  
+        <p>I used C++, Lua, and LuaBridge.</p>
+        </div>
+      </div>
+      </Link>
+    
+      <Link to="/pokemon_battle_sim">
+      <div className='game_article'>
+        <img src={battleCover} className = "cover"></img>
+        <div>
+        <h3>Pokemon Battle Simulator</h3>
+  
+        <p>I used R to create this project.</p>
+        </div>
+      </div>
+      </Link>
+
+      <Link to="/latchaw_website">
+      <div className='game_article'>
+        <img src={latchawCover} className = "cover"></img>
+        <div>
+        <h3>Website of Latchaw & Kilgore, CPA, P.C.</h3>
+        <p>I utilized wordpress to create their website. Showcases my abilities surrounding User Experience & Design.</p>
+      </div>
+      </div>
+      </Link>
+    </section>
+  </>
+  )
+}
+
+function Cinderella(){
+  return(
+
+  <>
+  <Header/>
+  <h1> Cinderella: A Visual Novel</h1>
+  <div className='GamePort'>
+  <iframe
+            src="/portfolio/cinderella/cinderella_v02/index.html"
+            width="960"
+            height="540"
+            style={{ border: 'none', display: 'block' }}
+            title="Cinderella Game"
+            allowFullScreen
+          />
+
+          <p>{cindDesc}</p>
+          </div>
+          </>
+  )
+}
+
+function BattleSim(){
+  return(
+    <>
+      <Header/>
+      <h1>Pokemon Battle Simulator</h1>
+      <div className='GamePort'>
+      <iframe 
+        src="https://e244on-victoria-kilgore.shinyapps.io/pokemon_battle_sim/"
+        width="960"
+        height="800"
+        style={{ border: 'none', display: 'block' }}
+        title="Pokemon Battle Simulator"
+        allowFullScreen
+      />
+      </div>
+    </>
+  )
+}
+
+function GameEngine(){
+  return (
+    <>
+    <Header/>
+    <h1> EECS 498: Game Engine </h1>
+    <div className='GamePort'>
+      <p>Uses: C++, LuaBridge, Lua, SDL2, Emscripten, GLM, RapidJSON</p>
+       <iframe 
+    src="/portfolio/game_engine/lollipopak_engine.html" 
+            width="1150"
+            height="540"
+            style={{ border: 'none', display: 'block' }}
+            title="LollipopAK Engine"
+            allowFullScreen>
+    </iframe>
+      
+    </div>
+    </>
+  )
 }
 
 export default function App() {
@@ -146,6 +282,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gamedev" element={<GameDev />} />
+        <Route path="/softwaredev" element={<Software />} />
+        <Route path="/cinderella" element={<Cinderella />} />
+        <Route path="/pokemon_battle_sim" element={<BattleSim />} />
+        <Route path="/lollipopak_engine" element={<GameEngine />} />
       </Routes>
     </HashRouter>
   )
